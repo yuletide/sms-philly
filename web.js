792b5330a4a5640e6e20b11d5d1185054b5ea4c8
Alex Yule <alexy@codeforamerica.org>
2012-02-11 00:55:08 -0500

    updated package for tropo

--- web.js
+++ web.js
@@ -1,7 +1,11 @@
-var express = require('express'),
-  tropowebapi = require('tropo-webapi');
+var express = require('express');
+//tropowebapi = require('tropo-webapi');
 
-var tropo = new tropowebapi.TropoWebAPI();
+//var tropo = new tropowebapi.TropoWebAPI();
+
+var gClient = '599070097566.apps.googleusercontent.com';
+var gSecret = 'N9LYDcK_KkGECvd4nzVQEqs_';
+var gRedirect = 'http://sms2gdocs.herokuapp.com/oauth2callback';
 
 var app = express.createServer(express.logger());
 
