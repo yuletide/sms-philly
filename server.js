ac1bdce7d922ff46b65e5c5cc44bbc3b5c36295f
Alex Yule <alexy@codeforamerica.org>
2012-02-12 09:57:07 -0500

    removed server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-var tropowebapi = require('tropo-webapi');
-var sys = require('sys');
-var tropo = new tropowebapi.TropoWebAPI();
-tropo.say("Hello, World.");
-sys.puts(tropowebapi.TropoJSON(tropo));
