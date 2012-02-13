be493a967761fdcabb968e35ae129d02b7269587
Alex Yule <alexy@codeforamerica.org>
2012-02-13 08:33:31 -0500

    trying to grab initialtext

--- web.js
+++ web.js
@@ -44,7 +44,7 @@ app.post('/sms.json', function(req, res){
   tropo.say("Thanks for your message!");
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.end(tropowebapi.TropoJSON(tropo));
-  console.log(req.body.session.initialText);
+  console.log(req.body.session);
 });
 
 var port = process.env.PORT || 3000;
